833515f3e5172084847f83457b402282d1e19ce2
Anil Zeybek <anil.zeybek98@gmail.com>
2022-05-05 21:46:28 +0300

    tune shell script

---
+++ tune.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+for s in 0 1
+do
+    python src/main.py --seed=$s --number_of_rbs=1
+
+    for norb in 5 9
+    do 
+        for cf in 10000 25000
+        do
+            for alpha in 0.6 0.8 0.9
+            do
+                python src/main.py --seed=$s --number_of_rbs=$norb --clustering_fq=$cf --alpha=$alpha
+            done
+        done
+    done
+done
