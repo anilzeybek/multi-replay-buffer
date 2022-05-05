22baccbadf31b44a9aa502e30ec4ebd7796a65a4
Anil Zeybek <anil.zeybek98@gmail.com>
2022-05-05 18:29:09 +0300

    rm hyperparams.json

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 import argparse
-import json
 import os
 import random
 from time import time
@@ -11,35 +10,44 @@ import torch
 from td3_agent import TD3Agent
 
 
-def read_hyperparams():
-    with open('src/hyperparams.json') as f:
-        hyperparams = json.load(f)
-        return hyperparams
-
-
 def get_args():
     parser = argparse.ArgumentParser(description='options')
     parser.add_argument('--env_name', type=str, required=True)
-    parser.add_argument('--mer', default=False, action='store_true')
-    parser.add_argument('--seed', type=int, default=0)
-
     parser.add_argument('--test', default=False, action='store_true')
     parser.add_argument('--cont', default=False, action='store_true', help="use already saved policy in training")
+    parser.add_argument('--seed', type=int, default=0)
+
+    parser.add_argument("--max_timesteps", type=int, default=int(1.5e+5))
+    parser.add_argument("--expl_noise", type=float, default=0.1)
+    parser.add_argument("--start_timesteps", type=int, default=10000)
+    parser.add_argument("--buffer_size", type=int, default=200000)
+    parser.add_argument("--actor_lr", type=float, default=1e-3)
+    parser.add_argument("--critic_lr", type=float, default=1e-3)
+    parser.add_argument("--batch_size", type=int, default=256)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--tau", type=float, default=0.005)
+    parser.add_argument("--policy_noise", type=float, default=0.2)
+    parser.add_argument("--noise_clip", type=float, default=0.5)
+    parser.add_argument("--policy_freq", type=int, default=2)
+    parser.add_argument("--mer", default=False, action='store_true')
+    parser.add_argument("--number_of_rbs", type=int, default=8)
+    parser.add_argument("--alpha", type=int, default=0.8)
+    parser.add_argument("--beta", type=int, default=0.5)
 
     args = parser.parse_args()
     return args
 
 
-def test(env, mer, seed):
+def test(env, args):
     agent = TD3Agent(
         obs_dim=env.observation_space.shape[0],
         action_dim=env.action_space.shape[0],
         action_bounds={"low": env.action_space.low, "high": env.action_space.high},
         env_name=env.unwrapped.spec.id,
-        mer=mer
+        mer=args.mer
     )
+    agent.load(args.seed)
 
-    agent.load(seed)
     scores = []
     for _ in range(1, 10):
         obs = env.reset()
@@ -58,32 +66,30 @@ def test(env, mer, seed):
         f.write(f"{np.array(scores).mean()}\n")
 
 
-def train(env, mer, cont, seed):
-    hyperparams = read_hyperparams()
-
+def train(env, args):
     agent = TD3Agent(
         obs_dim=env.observation_space.shape[0],
         action_dim=env.action_space.shape[0],
         action_bounds={"low": env.action_space.low, "high": env.action_space.high},
         env_name=env.unwrapped.spec.id,
-        expl_noise=hyperparams['expl_noise'],
-        start_timesteps=hyperparams['start_timesteps'],
-        buffer_size=hyperparams['buffer_size'],
-        actor_lr=hyperparams['actor_lr'],
-        critic_lr=hyperparams['critic_lr'],
-        batch_size=hyperparams['batch_size'],
-        gamma=hyperparams['gamma'],
-        tau=hyperparams['tau'],
-        policy_noise=hyperparams['policy_noise'],
-        noise_clip=hyperparams['noise_clip'],
-        policy_freq=hyperparams['policy_freq'],
-        mer=mer,
-        number_of_rbs=hyperparams['number_of_rbs'],
-        alpha=hyperparams['alpha'],
-        beta=hyperparams['beta']
+        expl_noise=args.expl_noise,
+        start_timesteps=args.start_timesteps,
+        buffer_size=args.buffer_size,
+        actor_lr=args.actor_lr,
+        critic_lr=args.critic_lr,
+        batch_size=args.batch_size,
+        gamma=args.gamma,
+        tau=args.tau,
+        policy_noise=args.policy_noise,
+        noise_clip=args.noise_clip,
+        policy_freq=args.policy_freq,
+        mer=args.mer,
+        number_of_rbs=args.number_of_rbs,
+        alpha=args.alpha,
+        beta=args.beta
     )
 
-    if cont:
+    if args.cont:
         agent.load()
 
     start = time()
@@ -91,7 +97,7 @@ def train(env, mer, cont, seed):
 
     obs = env.reset()
     score = 0
-    for i in range(1, int(2e+5)+1):
+    for i in range(1, args.max_timesteps+1):
         action = agent.act(obs)
         next_obs, reward, done, _ = env.step(action)
 
@@ -108,11 +114,9 @@ def train(env, mer, cont, seed):
             print(f"{i} | {scores[-1]:.2f}")
 
     end = time()
-    print("training completed, elapsed time: ", end - start)
-    print()
+    print(f"training completed, elapsed time: {end - start}\n")
 
-    agent.save(seed)
-
+    agent.save(args.seed)
     return scores
 
 
@@ -129,9 +133,9 @@ def main():
     print(f"env: {args.env_name} | mer: {args.mer} | seed: {args.seed}")
 
     if args.test:
-        test(env, args.mer, args.seed)
+        test(env, args)
     else:
-        scores = train(env, args.mer, args.cont, args.seed)
+        scores = train(env, args)
 
         def moving_average(a, n):
             ret = np.cumsum(a, dtype=float)
